717f5e3cecc33677f8cbe98c9d7408447b3100c1
Miguel N <125605381+ManL512@users.noreply.github.com>
2024-03-12 18:41:51 -0500

    cambios

---
+++ front-end/mediapp/lib/widgets/mi_contenedor.dart
@@ -0,0 +1,8 @@
+//import 'package:flutter/material.dart';
+
+// class MiContenedor extends StatelessWidget{
+//   @override 
+//   Widget build(BuildContext){
+//     return Container()
+//   }
+// }
