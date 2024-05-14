d5a6aab8cb924f08275bc996e1552ef84bccf556
jesusbonillaupb <80067532+jesusbonillaupb@users.noreply.github.com>
2024-05-13 21:03:53 -0500

    url cambiada

--- front-end/mediapp/lib/constant/utils.dart
+++ front-end/mediapp/lib/constant/utils.dart
@@ -1,5 +1,5 @@
 
 class Utils{
   //you add your port no that u define in node js
-  static String baseUrl="http://10.0.2.2:3000"; // here we define the base url
+  static String baseUrl="http://mediapp.bucaramanga.upb.edu.co:3000"; // here we define the base url
 }
